54a8283902051fd310646e8c926764b9ba0bb226
Brian <chumobrian@gmail.com>
2023-09-07 17:49:41 +0300

    feat: Add a script that imports and uses functions from calculator_1 to calculate script file argv arguments

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, div, mul
+from sys import exit, argv
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>\n", end="")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if argv[2] == '+':
+        res = a + b
+        print("{} {} {} = {}\n".format(a, argv[2], b, res), end="")
+    elif argv[2] == '-':
+        res = a - b
+        print("{} {} {} = {}\n".format(a, argv[2], b, res), end="")
+    elif argv[2] == '*':
+        res = a * b
+        print("{} {} {} = {}\n".format(a, argv[2], b, res), end="")
+    elif argv[2] == '/':
+        res = a / b
+        print("{} {} {} = {}\n".format(a, argv[2], b, res), end="")
+    else:
+        print("Unknown operator. Available operators: +, -, * and /\n", end="")
+        exit(1)
