42ded92b82e183cd13e8b2ec1a839db2da5a680f
Brian <chumobrian@gmail.com>
2023-09-07 13:09:30 +0300

    fix(core): Prevent code execution when imported
    - Updated the script to use if __name__ == __main__: construct
    - Ensures that the code is only executed when the script is run directly

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-rsum = add(a, b)
-print("{} + {} = {}\n".format(a, b, rsum), end="")
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    rsum = add(a, b)
+    print("{} + {} = {}\n".format(a, b, rsum), end="")
