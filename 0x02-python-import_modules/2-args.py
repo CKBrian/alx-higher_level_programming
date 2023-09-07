e462162bf8d9957736eb8632cef2ce7a5ec05162
Brian <chumobrian@gmail.com>
2023-09-07 14:32:58 +0300

    fix(core): handle case for 1 argument
    - Updated the script to print '1 argument' whenever argv is 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ argc = len(argv) - 1
 
 if argc == 0:
     print("{} arguments.\n".format(argc), end="")
+elif argc == 1:
+    print("{} argument:\n".format(argc), end="")
+    print("{}: {}\n".format(argc, argv[1]), end="")
 else:
     print("{} arguments:\n".format(argc), end="")
     for i in range(1, argc + 1):
