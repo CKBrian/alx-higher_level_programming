3a69003118022b24d11cb17270934d884241d64b
Brian <chumobrian@gmail.com>
2023-09-07 14:48:49 +0300

    fix: Updated the script to use if __name__ == __main__: construct

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
+if __name__ == "__main__":
+    argc = len(argv) - 1
 
-argc = len(argv) - 1
-
-if argc == 0:
-    print("{} arguments.\n".format(argc), end="")
-elif argc == 1:
-    print("{} argument:\n".format(argc), end="")
-    print("{}: {}\n".format(argc, argv[1]), end="")
-else:
-    print("{} arguments:\n".format(argc), end="")
-    for i in range(1, argc + 1):
-        print("{}: {}\n".format(i, argv[i]), end="")
+    if argc == 0:
+        print("{} arguments.\n".format(argc), end="")
+    elif argc == 1:
+        print("{} argument:\n".format(argc), end="")
+        print("{}: {}\n".format(argc, argv[1]), end="")
+    else:
+        print("{} arguments:\n".format(argc), end="")
+        for i in range(1, argc + 1):
+            print("{}: {}\n".format(i, argv[i]), end="")
