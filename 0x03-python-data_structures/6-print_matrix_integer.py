c5876a119880b068b167b7e3762fe0f8453910e3
Brian <chumobrian@gmail.com>
2023-09-09 12:38:22 +0300

    fix(core): Add {:d} formatting in a function that prints all integers of a matrix.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for x in i:
-            print("{}".format(x), end=" " if x != i[-1] else "")
+            print("{:d}".format(x), end=" " if x != i[-1] else "")
 
         print()
