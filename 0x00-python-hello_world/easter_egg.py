8e260f55bfa6e148c4267c5972d2971cbe1db8fc
Brian <chumobrian@gmail.com>
2023-09-11 11:36:04 +0300

    feat: Add a program that prints the zen of python by Tim Peters

---
+++ 0x00-python-hello_world/easter_egg.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def print_zenOfPy():
+    s2 = "If the implementation is "
+    s4 = " to explain, it"
+    str3 = " is better than "
+    print(f"""The Zen of Python, by Tim Peters
+
+Beautiful{str3}ugly.
+Explicit{str3}implicit.
+Simple{str3}complex.
+Complex{str3}complicated.
+Flat{str3}nested.
+Sparse{str3}dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now{str3}never.
+Although never is often better than *right* now.
+{s2}hard{s4}'s a bad idea.
+{s2}easy{s4} may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!""")
