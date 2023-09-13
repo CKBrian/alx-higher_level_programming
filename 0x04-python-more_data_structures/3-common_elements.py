50ac249ab1ed8722708ef2f25f7d5599be613af1
Brian <chumobrian@gmail.com>
2023-09-13 16:16:27 +0300

    feat: Add a function that returns a set of common elements in two sets.

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    setc = set_1.intersection(set_2)  # { a & b for a, b in zip(set_1, set_2)}
+    setc = set_1 & set_2  # { a & b for a, b in zip(set_1, set_2)}
     return setc
