0c8aa3b975f3c7951fd7efae9419bd24fa13ce78
Brian <chumobrian@gmail.com>
2023-09-25 22:00:55 +0300

    feat: Add a function that raises a name exception with a message.

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    class custom_msg(Exception):
-        def __init__(self, msg):
-            super().__init__(msg)
-    raise custom_msg(message)
+    raise NameError(message)
