ac9feed72e2fc2d6abd2b10ca898c95e5583e23c
Brian <chumobrian@gmail.com>
2023-09-25 16:18:53 +0300

    fix(core): Handle case: elements to print is  or less

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,11 +3,11 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
         for elem in my_list:
+            if x <= 0:
+                break
             print("{}".format(elem), end="")
             x -= 1
             i += 1
-            if x == 0:
-                break
     except TypeError:
         pass
     else:
