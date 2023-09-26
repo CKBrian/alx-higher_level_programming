208d38c80abc083ba94d8efa0e45c1975364ba02
Brian <chumobrian@gmail.com>
2023-09-26 17:41:55 +0300

    feat: Add a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """
+    This is a class representing a square
+
+    Attributes:
+        size (int): length of each side of a square
+    """
+    def __init__(self, size):
+        self._size = size
