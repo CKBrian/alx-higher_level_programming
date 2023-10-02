f68eea45339c393d6e3cd859126e40c423f117eb
Brian <chumobrian@gmail.com>
2023-10-02 16:52:07 +0300

    feat: Add all .py files for the python-more_classes project

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+    calculates the sum of two integer and float numbers
+"""
+def add_integer(a, b=98):
+    """
+		adds two integers and returns result
+		if b is not provided it defaults to 98
+	"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
