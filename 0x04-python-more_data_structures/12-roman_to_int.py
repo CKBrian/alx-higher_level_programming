f7078296f9f53329cd9736fb4ea14ee5193282d5
Brian <chumobrian@gmail.com>
2023-10-02 10:55:28 +0300

    feat: Add all files for Test driven development project

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None:
+        return None
     num = 0
     rome = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for numeral in roman_string:
+        if numeral not in rome:
+            return None
         num += rome[numeral]
     maxi = rome[roman_string[-1]]
-    if maxi > num - maxi:
-        num = maxi - (num - maxi)
+    for x in roman_string:
+        if maxi < rome[x]:
+            return int(num)
+    num = maxi - (num - maxi)
     return int(num)
