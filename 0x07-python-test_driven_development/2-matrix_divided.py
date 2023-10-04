8fad4ba330b66bb31f0b98f93ba769f2adac3528
Brian <chumobrian@gmail.com>
2023-10-04 05:57:51 +0300

    first commi

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+def matrix_divided(matrix, div):
+    """ divides all elements of a matrix """
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for ls in matrix:
+        if len(ls) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_matrix = [[round(x / div, 2) for x in lists] for lists in matrix]
+    return new_matrix
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+def text_indentation(text):
+    """
+        prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for char in text:
+        if char == '?' or char == '.' or char == ',':
+            print("{}\n\n".format(char),end="")
+        else:
+            print("{}".format(char), end="")
+    print("\n\n",end="")
