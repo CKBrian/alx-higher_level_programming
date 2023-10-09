e13884996ccfa8f5948e423e038e83effc710f77
Brian <chumobrian@gmail.com>
2023-10-09 22:28:30 +0300

    feat: Add a function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False.

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,8 +2,8 @@
 """ defines a subclass checking function"""
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """ returns True if the object is an instance of a class that
         inherited (directly or indirectly) from the specified class
         otherwise False"""
-    return issubclass(obj, a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
