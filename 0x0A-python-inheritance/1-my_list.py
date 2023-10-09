1657208dcff1069dab497e398295fa4cc40cc479
Brian <chumobrian@gmail.com>
2023-10-09 19:51:54 +0300

    feat: Add  a class MyList that inherits from list and corresponding doctest to test the class

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" defines a class Mylist that inherits from list"""
+
+
+class MyList(list):
+    """ MyList subclass """
+
+    def __init__(self):
+        """ initialises MyList subclass """
+        super().__init__()
+
+    def print_sorted(self):
+        """ print a sortend list of integers"""
+        super().sort()
+        print(self)
