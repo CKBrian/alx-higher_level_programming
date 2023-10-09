9f725d8c0e80f2942f336ccb4dafe044d0c01f6f
Brian <chumobrian@gmail.com>
2023-10-10 02:11:05 +0300

    feat: Add a class MyInt that inherits fron builtin int and has == and != operators inverted

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+""" defines a MyInt class"""
+
+
+class MyInt(int):
+    """ initializes an int class that inherits from builtin int"""
+
+    def __eq__(self, other):
+        """ inverts == to != """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """ inverts != to == """
+        return super().__eq__(other)
