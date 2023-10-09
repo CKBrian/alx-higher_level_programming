00a8ccf184fba9fa9c5d93ae00dd5e5be3931f3a
Brian <chumobrian@gmail.com>
2023-10-09 22:42:23 +0300

    feat: Add a function that raises an Exception 'area() is not implemented'

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+""" defines a class BaseGeometry. """
+
+
+class BaseGeometry():
+    """ initialise an BaseGeometry class """
+
+    def area(self):
+        """ raises an Exception with the message area() is not implemented """
+        raise Exception("area() is not implemented")
