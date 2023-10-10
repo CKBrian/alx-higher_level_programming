39efc144eb14f221e047e807d21466bd314023fe
Brian <chumobrian@gmail.com>
2023-10-10 21:24:09 +0300

    feat: Add a a class Student that defines a student with Public method def to_json(self): that retrieves a dictionary representation of a Student instance

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" ``9-student`` module that defines a class Student """
+
+
+class Student:
+    """ defines a class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """ initializes a class Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representation of a
+            Student instance """
+        return self.__dict__
