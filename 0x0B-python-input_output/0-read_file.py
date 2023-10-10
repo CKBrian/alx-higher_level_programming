346088afffee9fdb4bfefcc14ece96b45538f914
Brian <chumobrian@gmail.com>
2023-10-10 14:58:02 +0300

    feat: Add a  function that reads a text file (UTF8) and prints it to stdout:

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+""" defines a read_file module """
+
+
+def read_file(filename=""):
+    """ reads a text file (UTF8) and prints it to stdout: """
+    with open(filename) as myFile:
+        print(myFile.readlines())
