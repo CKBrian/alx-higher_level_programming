c8762463d620ce593a86a9debbddd0c7a0b16641
Brian <chumobrian@gmail.com>
2023-10-11 00:49:18 +0300

    fix(core): 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -21,5 +21,5 @@ class Student:
             Student instance """
         if not attrs:
             return self.__dict__
-        my_dict = self.__dict__
-        return {key: my_dict[key] for key in sorted(attrs) if key in my_dict}
+        s_dic = self.__dict__
+        return {key: s_dic.get("key") for key in sorted(attrs) if key in s_dic}
