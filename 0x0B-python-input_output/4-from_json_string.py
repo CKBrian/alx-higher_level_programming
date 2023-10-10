fe1750dd16bee6da712348a2b118f6f7995f272e
Brian <chumobrian@gmail.com>
2023-10-10 17:38:24 +0300

    feat: Add a function that returns an object (Python data structure) represented by a JSON string:

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" import json module for JSON related operations]"""
+from json import loads
+
+
+def from_json_string(my_str):
+    """ returns an object represented by a JSON string: """
+    return loads(my_str, ensure_ascii=False)
