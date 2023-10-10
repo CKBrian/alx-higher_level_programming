014822c1a3cc519742d9adf6a2743b6d4869b654
Brian <chumobrian@gmail.com>
2023-10-10 15:37:23 +0300

    feat: Add a function that writes a string to a text file (UTF8) and returns the number of characters written:

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+
+def write_file(filename="", text=""):
+    """ writes a string to a text file (UTF8) and
+    returns the number of characters written:"""
+    with open(filename, 'w', encoding="UTF8") as tFile:
+        return tFile.write(text)
