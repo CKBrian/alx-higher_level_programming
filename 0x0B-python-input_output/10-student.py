a2aa50f96767feb22a827a8ca5cb2de843feeed8
Brian <chumobrian@gmail.com>
2023-10-10 22:10:49 +0300

    feat: Add a a class Student that defines a student with Public method def to_jsonthat  retrieves a dictionary representation of a Student instance by specified attributes

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" ``9-student`` module that defines a class Student """
+
+
+class Student:
+    """ defines a class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """ initializes a class Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a
+            Student instance """
+        if not attrs:
+            return self.__dict__
+        my_dict = self.__dict__
+        return {key: my_dict[key] for key in sorted(attrs) if key in my_dict}
