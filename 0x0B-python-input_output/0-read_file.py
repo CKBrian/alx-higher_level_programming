02696032c4e50e39e77c0326ae92eecaabab3d07
Brian <chumobrian@gmail.com>
2023-10-10 15:06:43 +0300

    feat: Add a  function that reads a text file (UTF8) and prints it to stdout:

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,6 @@
 
 def read_file(filename=""):
     """ reads a text file (UTF8) and prints it to stdout: """
-    with open(filename, encoding="utf-8") as myFile:
-        print(myFile.read())
+    with open(filename, 'r', encoding="utf-8") as myFile:
+        for line in myFile:
+            print("{}".format(line))
