997ffb79329ac34023ddd712f578aba179839550
Brian <chumobrian@gmail.com>
2023-10-10 17:41:27 +0300

    feat: Add a function that returns an object (Python data structure) represented by a JSON string:

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,4 +5,4 @@ from json import loads
 
 def from_json_string(my_str):
     """ returns an object represented by a JSON string: """
-    return loads(my_str, ensure_ascii=False)
+    return loads(my_str)
