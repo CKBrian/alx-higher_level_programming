aeee4471aadb779377a194afda92dcb7b49e4e1f
Brian <chumobrian@gmail.com>
2023-10-10 17:48:52 +0300

    feat: Add a function that creates an Object from a “JSON file”:

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" defines a module ```6-load_from_json_file` """
+from json import load
+
+
+def load_from_json_file(filename):
+    """ creates an Object from a “JSON file” """
+    with open(filename, "r", encode="UTF-8") as f:
+        load(f)
