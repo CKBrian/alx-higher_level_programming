41c76b8612a6732dd4a9f8369a64719974a95fb0
Brian <chumobrian@gmail.com>
2023-10-11 23:00:03 +0300

    feat: Add a script that reads stdin line by line and computes metrics:

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""this test for input module"""
+from sys import stdin
+import signal
+
+
+my_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
+           '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+
+
+def SIGINT_handler(signal, frame):
+    """ handles signal interrupt """
+    print_dict()
+    exit(0)
+
+
+signal.signal(signal.SIGINT, SIGINT_handler)
+
+
+def print_dict():
+    """ prints the statistics """
+    global total_size, my_dict
+    print("File size: {}".format(total_size))
+    for key in my_dict:
+        if my_dict[key] > 0:
+            print("{}: {}".format(key, my_dict[key]))
+            my_dict[key] = 0
+
+
+def main():
+    """ gets input from input stream """
+    global total_size, my_dict
+    line_count = 0
+    try:
+        while True:
+            if not stdin.isatty():
+                for line in stdin:
+                    my_list = line.split()
+                    if str(my_list[-2]) in my_dict:
+                        key = str(my_list[-2])
+                        my_dict[key] = my_dict[key] + 1
+                    line_count = line_count + 1
+                    total_size = total_size + int(my_list[-1])
+                    if line_count == 10:
+                        print_dict()
+                        line_count = 0
+    except KeyboardInterrupt:
+        print("Input termination by CTRL-C")
+
+
+if __name__ == "__main__":
+    main()
