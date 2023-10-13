3027adc1b32b4e2cf82454315bec68d3dfe5fd87
Brian <chumobrian@gmail.com>
2023-10-13 16:10:06 +0300

    feat: Add a unittest file for the 6-max_integer module

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,4 +12,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertIsNone(max_integer([]))
         self.assertEqual(max_integer([9]), 9)
         self.assertEqual(max_integer([-1, 9]), 9)
-        self.assertEqual(max_integer([2, 4, 56, 566]), 566)
+        self.assertEqual(max_integer([-2, -4, -56, -566]), -2)
+        self.assertEqual(max_integer([1, 2, 3, 5]), 5)
+        self.assertEqual(max_integer([2, 4, 6, 5]), 6)
+        self.assertEqual(max_integer([32, 4, 6, 5]), 32)
