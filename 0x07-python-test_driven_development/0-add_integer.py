2a4ddacbb0fc9066ab34c049beb409769edeb855
Brian <chumobrian@gmail.com>
2023-10-13 15:18:45 +0300

    feat: Add a function that adds two integers along with relevant doctests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,13 +2,18 @@
 """
     calculates the sum of two integer and float numbers
 """
+
+
 def add_integer(a, b=98):
     """
-		adds two integers and returns result
-		if b is not provided it defaults to 98
-	"""
+        adds two integers and returns result
+        if b is not provided it defaults to 98
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+    result = a + b
+    if result == float('inf') or result == -float('inf'):
+        raise OverflowError("Result must be an integer")
     return int(a) + int(b)
