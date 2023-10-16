93ac41be7ce8f8ba74a33d07371a556607096f5d
Brian <chumobrian@gmail.com>
2023-10-16 22:21:42 +0300

    feat: add unittest for class Base

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" unittests for the base class """
+from unittest import TestCase
+from models.base import Base
+
+
+class TestBaseClass(TestCase):
+    def test_Attributes(self):
+        """ tests attributes """
+        pass
