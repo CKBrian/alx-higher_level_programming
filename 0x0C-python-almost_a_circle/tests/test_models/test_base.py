41d8821f8a9087674f35336850d0960fafb6114f
Brian <chumobrian@gmail.com>
2023-10-17 02:12:29 +0300

    feat: add unittest for class Base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,6 +5,26 @@ from models.base import Base
 
 
 class TestBaseClass(TestCase):
-    def test_Attributes(self):
+    def test_id(self):
         """ tests attributes """
-        pass
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
+
+        b5 = Base(3)
+        self.assertEqual(b5.id, 3)
+
+        b5 = Base()
+        self.assertEqual(b5.id, 4)
+
+
+if __name__ == "__main__":
+    unittest.main()
