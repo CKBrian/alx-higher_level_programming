f0b7563b92aed0adc93fa753d45b9c494d69c105
Brian <chumobrian@gmail.com>
2023-10-17 17:52:08 +0300

    chore: Update the base class unitest cases

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -2,6 +2,8 @@
 """ unittests for the base class """
 from unittest import TestCase
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBaseClass(TestCase):
@@ -20,6 +22,29 @@ class TestBaseClass(TestCase):
 
         b5 = Base(3)
         self.assertEqual(b5.id, 3)
+
+    def test_to_json_string(self):
+        """ tests to_json_string static method """
+        json_str = Base.to_json_string(None) #exists
+        self.assertEqual("[]", json_str)
+
+        json_str = Base.to_json_string([]) #exists
+        self.assertEqual("[]", json_str)
+
+        json_str = Base.to_json_string([ {'id': 12} ]) #returning a string exists
+        self.assertIsInstance(json_str, str)
+        self.assertEqual('[{"id": 12}]', json_str)
+
+    def test_from_json_string(self):
+        """ tests from_json_string static method """
+        json_list = Base.from_json_string(None) #exists
+        self.assertEqual([], json_list)
+
+        json_list = Base.from_json_string("[]") #exists
+        self.assertEqual([], json_list)
+
+        json_list = Base.from_json_string('[{ "id": 89 }]') #exists
+        self.assertEqual([{ 'id': 89 }], json_list)
 
 
 if __name__ == "__main__":
