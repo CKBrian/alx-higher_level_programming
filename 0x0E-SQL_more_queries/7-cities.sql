cbfc8c1005463e6b6fcbb6b891d095772756efe6
Brian <chumobrian@gmail.com>
2023-11-15 22:34:15 +0300

    chore: Add a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) cities id INT unique, auto generated, not null and is a primary key, state_id INT, not null and must be a FOREIGN KEY that references to id of the states table, name VARCHAR(256) not null

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,4 +2,4 @@
 -- cities id INT unique, auto generated, not null and is a primary key, state_id INT, not null and must be a FOREIGN KEY that references to id of the states table, name VARCHAR(256) not null
 CREATE DATABASE IF NOT EXISTS hbtn_od_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id));
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
