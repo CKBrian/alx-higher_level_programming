72fab94522bcb58b3735195b05beecdfb6fc38dc
Brian <chumobrian@gmail.com>
2023-11-15 21:35:16 +0300

    chore: Add a script that creates the table unique_id on MySQL server with id INT with the default value 1 and  unique and name VARCHAR(256)

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
---  creates the table unique_id on MySQL server with id INT with the default value 1 and  unique and name VARCHAR(256)
-CREATE TABLE uniqueid (id INT DEFAULT 1 UNIQUE, name VARCHAR(256))
+-- creates the table unique_id on MySQL server with id INT with the default value 1 unique and name VARCHAR(256)
+CREATE TABLE unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256))
