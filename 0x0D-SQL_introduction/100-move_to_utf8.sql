dfa2ee719a401fa12b0c77aee515bc27247c88fc
Brian <chumobrian@gmail.com>
2023-11-17 02:25:47 +0300

    chore: Add a script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci including thr Table first_table and Field name in first_table

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,7 +6,7 @@ ALTER DATABASE
 	COLLATE = utf8mb4_unicode_ci;
 ALTER TABLE
     first_table
-	CONVERT TO CHARACTER SET utf8mb4
+	-- CONVERT TO CHARACTER SET utf8mb4
 	COLLATE utf8mb4_unicode_ci;
 ALTER TABLE
     first_table
