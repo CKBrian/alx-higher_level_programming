9373062d3fe9fafc288363403116c57f9950a9a9
Brian <chumobrian@gmail.com>
2023-11-16 16:25:28 +0300

    chore: Add an SQL query that lists all cities contained in the database hbtn_0d_usa. - Each record displays: cities.id - cities.name - states.name sorted in ascending order by cities.id using one SELECT statement

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
--- creates the MySQL server user user_0d_1.
--- user_0d_1 has all privileges on MySQL server
--- The user_0d_1 password set to user_0d_1_pwd
--- If the user user_0d_1 already exists, script does not fail
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
-FLUSH PRIVILEGES;
+-- lists all cities contained in the database hbtn_0d_usa.
+-- Each record displays: cities.id - cities.name - states.name sorted in ascending order by cities.id using one SELECT statement
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
