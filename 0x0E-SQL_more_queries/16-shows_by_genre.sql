cca4fc5ce0e0c24bc7813178a31e39a6f05b9233
Brian <chumobrian@gmail.com>
2023-11-16 23:06:16 +0300

    chore: Add a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,10 @@
--- lists all the cities of California that can be found in the database hbtn_0d_usa
--- The states table contains only one record where name = California (but the id can be different, as per the example)
---Results must be sorted in ascending order by cities.id
-SELECT id, name
-FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY cities.id ASC;
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+-- If a show doesn’t have a genre, display NULL in the genre column
+-- Each record should display: tv_shows.title - tv_genres.name
+-- Results must be sorted in ascending order by the show title and genre name
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_shows.title, tv_genres.name
+ORDER BY tv_shows.title, tv_genres.name;
