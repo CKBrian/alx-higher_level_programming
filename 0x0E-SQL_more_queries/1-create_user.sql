39b01eca2c1c1e945a73aa3a9ff81a2d712889a8
Brian <chumobrian@gmail.com>
2023-11-16 14:25:16 +0300

    chore: Add a script that  creates the MySQL server user user_0d_1. with all privileges on MySQL server The user_0d_1 password set to user_0d_1_pwd

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,6 +2,6 @@
 -- user_0d_1 has all privileges on MySQL server
 -- The user_0d_1 password set to user_0d_1_pwd
 -- If the user user_0d_1 already exists, script does not fail
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVIlEGES ON *.* TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
