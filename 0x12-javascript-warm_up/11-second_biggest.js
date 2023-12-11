6f6de422d32b8fd8a8ce09ba9307639c985f75d3
Brian <chumobrian@gmail.com>
2023-12-11 23:53:24 +0300

    chore: Add  a script that prints the second largest number in a the argument list

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,8 +6,8 @@ all arguments can be converted to integer
 prints 0 if no argument passed
 prints 0 if the number of arguments is 1
  */
-if (process.argv.length === 2 || process.argv.length === 3) {
-  console.log(0);
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
   const sortedList = process.argv.slice(2).sort();
   const item = sortedList.length - 2;
