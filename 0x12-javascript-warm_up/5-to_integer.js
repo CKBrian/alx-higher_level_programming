2ea346d8e37a593a3a84c50f7ca0ad160f8e76f8
Brian <chumobrian@gmail.com>
2023-12-11 20:57:07 +0300

    chore: Add  a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,8 +4,8 @@
 
 If the argument can’t be converted to an integer, print “Not a number”
  */
-const argv1 = process.argv[2];
-if (typeof (Number(argv1)) === 'number') {
+const argv1 = Number(process.argv[2]);
+if (typeof (argv1) === 'number' && !isNaN(argv1)) {
   console.log(`My number: ${argv1}`);
 } else {
   console.log('Not a number');
