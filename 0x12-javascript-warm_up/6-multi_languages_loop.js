a039db3a4b495a3c3b53fe78993a6123d5561fe2
Brian <chumobrian@gmail.com>
2023-12-11 21:06:08 +0300

    chore: Add  a script that prints three lines of text

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* prints using an array of string and a loop
+The first line: “C is fun”
+The second line: “Python is cool”
+The third line: “JavaScript is amazing”
+ */
+const arr1 = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(`${arr1[i]}`);
+}
