c581e6d0edd378f8463f5782f15f5ce711a289d0
Brian <chumobrian@gmail.com>
2023-12-12 00:01:36 +0300

    chore: Add  a function that returns the sum of two numbers”

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ prints the addition of 2 integers
+
+The first argument is the first integer
+The second argument is the second integer
+You have to define a function with this prototype: function add(a, b)
+ */
+exports.add = function (a, b) {
+  return a + b;
+};
