e0772243dcd3299a9cc00f10d30556301abe9914
Brian <chumobrian@gmail.com>
2023-12-11 22:38:33 +0300

    chore: Add  a script that prints a square”

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ prints a square
 The first argument is the size of the square
 print “Missing size” if the first argument can’t be converted to an integer
  */
-const sideLength = Number(process.argv[2]);
+const sideLength = parseInt(process.argv[2]);
 
 if (!isNaN(sideLength)) {
   for (let i = 0; i < sideLength; i++) {
