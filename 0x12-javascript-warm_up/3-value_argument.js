a6f190bfad772bc5cf2a752ce20caa7a2bb1ec62
Brian <chumobrian@gmail.com>
2023-12-11 20:12:25 +0300

    chore: Add  a script that  prints the first argument passed to it

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+ prints the first argument passed to it:
+
+If no arguments are passed to the script, prints “No argument”
+You must use console.log(...) to prints all output
+ */
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else {
+  console.log(`${process.argv[2]}`);
+}
