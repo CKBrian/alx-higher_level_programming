84b0dac4deec83a23592208d6662ba5b85a707b6
Brian <chumobrian@gmail.com>
2023-12-11 22:37:26 +0300

    chore: Add  a script that returns the sum of tw numbers”

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -9,8 +9,6 @@ You have to define a function with this prototype: function add(a, b)
 function add (a, b) {
   return a + b;
 }
-const num1 = Number(process.argv[2]);
-const num2 = Number(process.argv[3]);
-if (!isNaN(num1) || !isNaN(num2)) {
-  console.log(`${add(num1, num2)}`);
-}
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+console.log(`${add(num1, num2)}`);
