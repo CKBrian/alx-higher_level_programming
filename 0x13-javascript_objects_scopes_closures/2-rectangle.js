05c73a7830b2e42b6ee51430d0025acc2b588c21
Brian <chumobrian@gmail.com>
2023-12-12 17:25:16 +0300

    chore: Add a function that creates a Rectangle class with constructor that takes 2 arguments w and h, validates and assigns to width and height variables

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
-      this.width = undefined;
-      this.height = undefined;
-    } else {
+    if (!isNaN(w) && !isNaN(h) && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
