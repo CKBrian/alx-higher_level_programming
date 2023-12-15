0c5c6f94c70a7a674a8ed3eba50de633e99a6870
Brian <chumobrian@gmail.com>
2023-12-15 17:09:39 +0300

    feat(database): add script that lists states from hbtn_0e_0_usa using MySQLdb

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# lists all states from the database hbtn_0e_0_usa
+# Uses the module MySQLdb (import MySQLdb) to connect to  MySQL
+# server on localhost at port 3306
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # creates connection to the mySQL server
+    conn = MySQLdb.connect(user=username, host="localhost",
+                           passwd=password, db=db_name, charset="utf8")
+
+    cursor = conn.cursor()  # cursor object to execute queries
+
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")  # SQL query
+
+    states_rows = cursor.fetchall()  # fetches the table rows
+
+    for state in states_rows:
+        print("({}, '{}')".format(state[0], state[1]))
+
+    cursor.close()
+    conn.close()
