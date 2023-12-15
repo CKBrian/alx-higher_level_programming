986acb0125958c3f74b5142e9cd45e5b899b6057
Brian <chumobrian@gmail.com>
2023-12-16 01:26:23 +0300

    feat(database): Add a script that Defines a module that  takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Defines a module that  takes in an argument and displays all
+values in the states table of hbtn_0e_0_usa where name matches the argument
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    usr, password, db_name, state_name = sys.argv[1:5]
+
+    conn = MySQLdb.connect(host="localhost", charset="utf8", user=usr,
+                           passwd=password, db=db_name)
+
+    cursor = conn.cursor()
+    query = "SELECT * FROM states WHERE name = '{}'" \
+            "ORDER BY states.id ASC".format(state_name)
+    cursor.execute(query)
+
+    state_rows = cursor.fetchall()
+    for state in state_rows:
+        print("({}, '{}')".format(state[0], state[1]))
+
+    cursor.close()
+    conn.close()
