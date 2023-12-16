18683c7e8664df2f14609f259ea99b8413aa519b
Brian <chumobrian@gmail.com>
2023-12-16 22:40:21 +0300

    feat(datatbase): Add a script that Defines a module that prints the first State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Defines a module that prints the first State object
+from the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    user, passwd, db = sys.argv[1:4]
+    engn = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, passwd, db)
+    engine = create_engine(engn, pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 1).first()
+    print("{}: {}".format(state.id, state.name))
+
+    session.close()
