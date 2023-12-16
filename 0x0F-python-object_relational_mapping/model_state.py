8d670f0a77f31ef0775f575c090aae3c5a316bdf
Brian <chumobrian@gmail.com>
2023-12-16 16:01:15 +0300

    feat(database): Add a script that Defines a module with a class definition of a State and an instance Base = declarative_base()

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
 """
 Defines a module which contains the class definition of a
-State and an instance Base = declarative_base()
+State and an instance Base of declarative_base
 """
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 import sys
 
-
-user = sys.argv[1]
-passwd = sys.argv[2]
-db = sys.argv[3]
-
-# Create an engine to connect to database
-engn = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, passwd, db)
-engine = create_engine(engn, pool_pre_ping=True)
 
 Base = declarative_base()
 
@@ -26,10 +18,3 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
-
-
-Base.metadata.create_all(engine)  # Create table
-
-# Create a new session
-Session = sessionmaker(bind=engine)
-session = Session()
