4a7cee8360fac912c205f5d4865ecd622046c301
Brian <chumobrian@gmail.com>
2023-12-17 20:38:32 +0300

    feat(datatbase): Add a script that Defines a module that prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -21,5 +21,7 @@ if __name__ == "__main__":
     state = session.query(State).filter(State.id == 1).first()
     if state:
         print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
 
     session.close()
