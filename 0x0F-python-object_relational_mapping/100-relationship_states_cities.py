350801ab7b63e92333a790902f5772a48b7ef34a
Brian <chumobrian@gmail.com>
2023-12-18 11:11:57 +0300

    feat(database): Add a csript that defines a module that  creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Defines a module that  creates the State “California” with the
+City “San Francisco” from the database hbtn_0e_100_usa
+"""
+import sys
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City, Base
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    user, passwd, db = sys.argv[1:4]
+    engn = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, passwd, db)
+    engine = create_engine(engn, pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state1 = State(name = "California")
+    city1 = City(name = "San Francisco")
+    state1.cities.append(city1)
+    session.add(state1)
+    session.commit()
+
+    session.close()
