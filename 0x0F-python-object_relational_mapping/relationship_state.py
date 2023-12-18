fae802820dc8f4fc2c1b5ad0196743c085f0e8a6
Brian <chumobrian@gmail.com>
2023-12-18 11:10:44 +0300

    feat(database): Add a script that defines a module which contains the class definition of a State and an instance Base of declarative_base

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Defines a module which contains the class definition of a
+State and an instance Base of declarative_base
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ Defines the structure of the states table in hbtn_0e_6_usa """
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state",
+                          cascade="all, delete-orphan")
