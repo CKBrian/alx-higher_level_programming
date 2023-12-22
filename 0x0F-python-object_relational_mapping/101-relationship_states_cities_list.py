b01af0ba4cebc4ae76b7e2f038b89012584bca83
Brian <chumobrian@gmail.com>
2023-12-22 23:25:49 +0300

    feat(database): Add a script that lists all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Defines a module that  lists all State objects, and
+corresponding City objects in the database hbtn_0e_100_usa
+"""
+import sys
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City, Base
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    user, passwd, db = sys.argv[1:4]
+    engn = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, passwd, db)
+    engine = create_engine(engn, pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("        {}: {}".format(city.id, city.name))
+
+    session.close()
