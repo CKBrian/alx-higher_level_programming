12890839e0dc48ebf9da4c2cb210b63c788c949e
Brian <chumobrian@gmail.com>
2024-01-05 17:35:50 +0300

    feat: Add a script that takes in a URL and displays all HTTP methods the server will accept

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -sIX ALLOW "$1" | grep allow: | tr -d ' ' | cut -d ':' -f2 | sed 's/,/, /g'
+curl -sI ALLOW "$1" | grep allow: | tr -d ' ' | cut -d ':' -f2 | sed 's/,/, /g'
