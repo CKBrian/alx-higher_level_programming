8148329c7ac8398fa566e4fd31911e61ab4d6e96
Brian <chumobrian@gmail.com>
2024-01-06 15:31:33 +0300

    feat: Add a script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a module that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8)."""
+import requests
+from requests.exceptions import HTTPEerror
+import sys
+
+
+if __name__ == "__main__":
+    """ takes in a URL, sends a request to the URL and
+    displays the body of the response"""
+    url = sys.argv[1]
+    try:
+	    resp = request.get(url)
+        print(resp.text)
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
