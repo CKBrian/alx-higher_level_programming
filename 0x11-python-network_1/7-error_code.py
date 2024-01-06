3bc80383982e704ab1786b3dc7bcd177612cf42d
Brian <chumobrian@gmail.com>
2024-01-06 20:43:47 +0300

    feat: Add a script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,11 +12,12 @@ if __name__ == "__main__":
     url = sys.argv[1]
     try:
         resp = requests.get(url)
-        print(resp.text)
-    except HTTPError as e:
         code = e.response.status_code
+
         if code >= 400:
             print("Error code: {}".format(code))
         else:
             print(resp.text)
+    except HTTPError as e:
+        code = e.response.status_code
 
