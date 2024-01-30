85336afaee1381f1a74a70029c9d26ca8c86e054
Brian <chumobrian@gmail.com>
2024-01-31 01:17:30 +0300

    feat: Add a script that computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -15,7 +15,6 @@ request(url, (err, resp, body) => {
   const list = JSON.parse(body);
   for (let i = 1; i < 11; i++) {
     const uId = i;
-	//{ '1': 11,
     if (i === 1) {
       console.log(`{\t'${uId}': ${userList(list, uId)},`);
     } else if (i === 10) {
