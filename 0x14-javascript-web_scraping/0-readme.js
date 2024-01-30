24eb8342e2d14de79bbbc46978fe5efe24e2310a
Brian <chumobrian@gmail.com>
2024-01-30 13:47:42 +0300

    feat: Add a script that reads the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 const filename = process.argv[2];
-console.log(filename);
 fs.readFile(filename, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
